2f4f39b5a152135640466734f04a11cce72f8446
rajendraf5 <princesvishwakarmaf5@gmail.com>
2021-03-27 15:31:09 +0530

    js upate

--- assets/js/video-background.js
+++ assets/js/video-background.js
@@ -22,7 +22,7 @@
     // of different video formats to add
     src: [
       {
-        src: 'assets/images/small.ogv',
+        src: 'assets/images/atrangivideo.mp4',
         type: 'video/mp4'
       }
     ],
