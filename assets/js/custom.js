1c14b56a61effd813d022dd1c71a1fef63557423
rajendraf5 <68588129+rajendraf5@users.noreply.github.com>
2021-03-27 15:37:02 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -51,6 +51,59 @@ jQuery(document).ready(function( $ ) {
   //     }
   //   });
   // });
+
+  $(document).ready(function() {
+  
+  setTimeout(function(){
+    $('body').addClass('loaded');
+    
+  }, 3000);
+  
+});
+ 
+    $(document).ready(function() {
+  
+  typing( 0, $('.typewriter-text').data('text') );
+
+  function typing( index, text ) {
+    
+    var textIndex = 1;
+
+    var tmp = setInterval(function() {
+      if ( textIndex < text[ index ].length + 1 ) {
+        $('.typewriter-text').text( text[ index ].substr( 0, textIndex ) );
+        textIndex++;
+      } else {
+        setTimeout(function() { deleting( index, text ) }, 2000);
+        clearInterval(tmp);
+      }
+
+    }, 150);
+
+  }
+
+  function deleting( index, text ) {
+
+    var textIndex = text[ index ].length;
+
+    var tmp = setInterval(function() {
+
+      if ( textIndex + 1 > 0 ) {
+        $('.typewriter-text').text( text[ index ].substr( 0, textIndex ) );
+        textIndex--;
+      } else {
+        index++;
+        if ( index == text.length ) { index = 0; }
+        typing( index, text );
+        clearInterval(tmp);
+      }
+
+    }, 150)
+
+  }
+
+});
+
 });
 
  $(".owl-carousel").each(function (index) {
@@ -163,5 +216,3 @@ jQuery(document).ready(function( $ ) {
         a(".navbar-toggle:visible").click();
     });
 }(jQuery);
-
-
